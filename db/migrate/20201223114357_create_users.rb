76275006f4436ae303eee0efa1aa857d0f38109a
naoki.kubota <nk.0110.6@gmail.com>
2020-12-23 11:44:14 +0000

    model

---
+++ db/migrate/20201223114357_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.string :image
+      t.text :profile
+
+      t.timestamps
+    end
+  end
+end
